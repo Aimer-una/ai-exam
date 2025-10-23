24c3d9beaa115f55ea6b3b65fd5bb2e06f72df1a
chenxingyong <2103095120@qq.com>
2025-10-23 17:56:27 +0800

    轮播图模块文件上传接口

--- src/main/java/com/atguigu/exam/controller/BannerController.java
+++ src/main/java/com/atguigu/exam/controller/BannerController.java
@@ -6,6 +6,7 @@ import com.atguigu.exam.entity.Banner;
 import com.atguigu.exam.service.BannerService;
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.core.conditions.update.LambdaUpdateWrapper;
+import io.minio.errors.*;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -13,6 +14,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
 import java.util.List;
 import java.util.Map;
 
@@ -38,9 +42,9 @@ public class BannerController {
     @Operation(summary = "上传轮播图图片", description = "将图片文件上传到MinIO服务器，返回可访问的图片URL")  // API描述
     public Result<String> uploadBannerImage(
             @Parameter(description = "要上传的图片文件，支持jpg、png、gif等格式，大小限制5MB") 
-            @RequestParam("file") MultipartFile file) {
-
-        return Result.success("上传图片地址", "图片上传成功");
+            @RequestParam("file") MultipartFile file) throws ServerException, InsufficientDataException, ErrorResponseException, IOException, NoSuchAlgorithmException, InvalidKeyException, InvalidResponseException, XmlParserException, InternalException {
+        String fileUrl = bannerService.getFileUrl(file);
+        return Result.success(fileUrl, "图片上传成功");
     }
     
     /**
--- src/main/java/com/atguigu/exam/service/impl/BannerServiceImpl.java
+++ src/main/java/com/atguigu/exam/service/impl/BannerServiceImpl.java
@@ -4,16 +4,48 @@ import com.atguigu.exam.entity.Banner;
 import com.atguigu.exam.mapper.BannerMapper;
 import com.atguigu.exam.service.BannerService;
 
+import com.atguigu.exam.service.FileUploadService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import io.minio.errors.*;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.ObjectUtils;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
-import java.time.LocalDateTime;
-import java.util.List;
-
+import java.io.IOException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
 /**
  * 轮播图服务实现类
  */
 @Service
+@Slf4j
 public class BannerServiceImpl extends ServiceImpl<BannerMapper, Banner> implements BannerService {
 
-} 
+    @Autowired
+    private FileUploadService fileUploadService;
+    @Override
+    public String getFileUrl(MultipartFile file) throws ServerException, InsufficientDataException, ErrorResponseException, IOException, NoSuchAlgorithmException, InvalidKeyException, InvalidResponseException, XmlParserException, InternalException {
+        // 判断上传的文件是否为空
+        if (ObjectUtils.isEmpty(file)) {
+            throw new RuntimeException("请选择要上传的文件");
+        }
+        // 判断文件的类型是否正确
+        String contentType = file.getContentType();
+        if (StringUtils.isEmpty(contentType) || !contentType.startsWith("image")){
+            throw new RuntimeException("请选择正确格式的文件");
+        }
+
+        // 文件大小限制
+        if (file.getSize() > 5 * 1024 * 1024){
+            throw new RuntimeException("上传的文件不可以超过5MB");
+        }
+
+        // 调用上传文件的业务
+        String url = fileUploadService.uploadFile(file, "banner");
+        log.info("完成banner图片上传，图片回显地址：{}",url);
+        return url;
+    }
+}
