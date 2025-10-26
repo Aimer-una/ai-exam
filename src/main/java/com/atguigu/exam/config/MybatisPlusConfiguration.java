40a74451ed9799a02a299fade25a513f8e8622b0
chenxingyong <2103095120@qq.com>
2025-10-26 12:01:07 +0800

    配置mybatisplus分页插件

--- src/main/java/com/atguigu/exam/config/MybatisPlusConfiguration.java
+++ src/main/java/com/atguigu/exam/config/MybatisPlusConfiguration.java
@@ -1,6 +1,10 @@
 package com.atguigu.exam.config;
 
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
 import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 /**
@@ -12,5 +16,12 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 @MapperScan("com.atguigu.exam.mapper")
 public class MybatisPlusConfiguration {
-
+    // 配置MybatisPlus插件
+    @Bean
+    public MybatisPlusInterceptor mybatisPlusInterceptor(){
+        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        // 添加分页插件
+        interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
+        return interceptor;
+    }
 }
