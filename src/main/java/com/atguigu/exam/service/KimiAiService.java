6e5098f3ed159b717070c5f3ea2b070dfe637386
chenxingyong <2103095120@qq.com>
2025-10-29 17:20:25 +0800

    kimi提示词设置

--- src/main/java/com/atguigu/exam/service/KimiAiService.java
+++ src/main/java/com/atguigu/exam/service/KimiAiService.java
@@ -1,10 +1,13 @@
 package com.atguigu.exam.service;
 
 
+import com.atguigu.exam.vo.AiGenerateRequestVo;
+
 /**
  * Kimi AI服务接口
  * 用于调用Kimi API生成题目
  */
 public interface KimiAiService {
+    String buildPrompt(AiGenerateRequestVo request);
 
 } 
