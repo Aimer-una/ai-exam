cb3c4fb7d6b1410b4e483a51f5480560d554f6d2
chenxingyong <2103095120@qq.com>
2025-10-29 17:37:58 +0800

    kimi基础信息配置

---
+++ src/main/java/com/atguigu/exam/config/properties/KimiProperties.java
@@ -0,0 +1,13 @@
+package com.atguigu.exam.config.properties;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties(prefix = "kimi.api")
+@Data
+public class KimiProperties {
+    private String model;
+    private String url;
+    private String apiKey;
+    private Double temperature;
+}
