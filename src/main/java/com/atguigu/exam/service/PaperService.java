1eab3d7419994a4b54cfce9152cc634391ea7195
chenxingyong <2103095120@qq.com>
2025-10-30 15:47:16 +0800

    获取试卷列表接口

--- src/main/java/com/atguigu/exam/service/PaperService.java
+++ src/main/java/com/atguigu/exam/service/PaperService.java
@@ -3,9 +3,12 @@ package com.atguigu.exam.service;
 import com.atguigu.exam.entity.Paper;
 import com.baomidou.mybatisplus.extension.service.IService;
 
+import java.util.List;
+
 /**
  * 试卷服务接口
  */
 public interface PaperService extends IService<Paper> {
 
+    List<Paper> getPaperList(String name, String status);
 } 
--- src/main/java/com/atguigu/exam/service/impl/PaperServiceImpl.java
+++ src/main/java/com/atguigu/exam/service/impl/PaperServiceImpl.java
@@ -4,9 +4,13 @@ package com.atguigu.exam.service.impl;
 import com.atguigu.exam.entity.Paper;
 import com.atguigu.exam.mapper.PaperMapper;
 import com.atguigu.exam.service.PaperService;
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.ObjectUtils;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 
 /**
@@ -17,4 +21,11 @@ import org.springframework.stereotype.Service;
 public class PaperServiceImpl extends ServiceImpl<PaperMapper, Paper> implements PaperService {
 
 
-} 
+    @Override
+    public List<Paper> getPaperList(String name, String status) {
+        LambdaQueryWrapper<Paper> lambdaQueryWrapper = new LambdaQueryWrapper<>();
+        lambdaQueryWrapper.like(!ObjectUtils.isEmpty(name),Paper::getName,name);
+        lambdaQueryWrapper.eq(!ObjectUtils.isEmpty(status),Paper::getStatus,status);
+        return list(lambdaQueryWrapper);
+    }
+}
